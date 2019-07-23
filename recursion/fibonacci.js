3c012b937459e59fe792283fe8fc7b98236c6d25
montekaka <josh.chen.jia@gmail.com>
2019-07-23 10:55:23 -0700

    add exercises

--- recursion/fibonacci.js
+++ recursion/fibonacci.js
@@ -8,3 +8,18 @@ output   0 1 1 2 3 5 8 13 21 34 ...
 
 What is the time complexity? Can you think of optimizing your solution? (Hint: look up dynamic programming)
 */
+
+// const fibonnaci = (num) => {
+//   if(num <= 1) return num;
+//   return fibonnaci(num - 1) + fibonnaci(num - 2);
+// }
+
+const fibonnaci = (num) => {
+  const f = [0, 1];
+  for(var i = 2; i <= num; i++) {
+    f[i] = f[i-1] + f[i-2];
+  }
+  return f[num];
+}
+
+console.log(fibonnaci(1))
--- recursion/reverse.js
+++ recursion/reverse.js
@@ -4,3 +4,25 @@ Implement a function that will reverse a string recursively.
 reverse('abcdefg')
 => 'gfedcba'
 */
+
+
+// const reverse = (str) => {
+//   let start = 0;
+//   let end = str.length - 1;
+//   let result = []
+//   while(start < end) {
+//     const temp = str[start];
+//     result[start] = str[end];
+//     result[end] = temp;    
+//     start += 1;
+//     end -=1;
+//   }
+//   return result.join('');
+// }
+
+const reverse = (str) => {
+  if(str.length === 0) return str;
+  return reverse(str.slice(1)) + str[0];
+}
+
+console.log(reverse('abcdefg'));
